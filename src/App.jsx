a184330eed4a08f1a7d7cf9ffa8a1826a9700a6b
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-15 14:06:34 +0530

    added routing and created basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react';
-import NavBar from './components/NavBar';
+import Body from './components/Body';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <NavBar />
-      <h1 className="text-3xl font-bold ">
-        Hello world!
-      </h1>
+     <BrowserRouter basename='/'>
+        <Routes>
+          <Route path="/" element={<><Body/></>}>
+             <Route path="/login" element={<><Login /></>} />
+             <Route path="/profile" element={<><Profile /></>} />
+          </Route>
+        </Routes>
+     </BrowserRouter>
+
+    
     </>
   )
 }
