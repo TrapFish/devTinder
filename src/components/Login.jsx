dcbef7718833cd481dfab6e7a61a4c1e2a6d46e4
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-17 15:29:29 +0530

    add cahce settings

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const Login = () => {
@@ -7,21 +7,21 @@ const Login = () => {
         password: ''
     })
 
-    const handleChange = (e)=>{
-       setLogin((prev)=>{
-        return {...prev, [e.target.name]: e.target.value}
-       })
+    const handleChange = (e) => {
+        setLogin((prev) => {
+            return { ...prev, [e.target.name]: e.target.value }
+        })
     }
 
-    const handleSubmit = async ()=> {
+    const handleSubmit = async () => {
         try {
             console.log("Line 17 ::", login);
-           const data = await axios.post("http://localhost:3000/login", login, {withCredentials: true});
-           console.log("Line 20 ::", data)
+            const data = await axios.post("http://localhost:3000/login", login, { withCredentials: true });
+            console.log("Line 20 ::", data)
         } catch (error) {
             console.log("Error ::", error)
         }
-        
+
     }
 
     return (
@@ -37,16 +37,16 @@ const Login = () => {
                                 <div className="label">
                                     <span className="label-text">Email ID : </span>
                                 </div>
-                                <input type="email"  className="input input-bordered w-full max-w-xs" name="emailId" value={login?.emailId} onChange={handleChange}/>
-                                
+                                <input type="email" className="input input-bordered w-full max-w-xs" name="emailId" value={login?.emailId} onChange={handleChange} />
+
                             </label>
 
                             <label className="form-control w-full max-w-xs my-4">
                                 <div className="label">
                                     <span className="label-text">Password : </span>
                                 </div>
-                                <input type="password"  className="input input-bordered w-full max-w-xs" name="password" value={login?.password} onChange={handleChange}/>
-                                
+                                <input type="password" className="input input-bordered w-full max-w-xs" name="password" value={login?.password} onChange={handleChange} />
+
                             </label>
                         </div>
                         <div className="card-actions justify-center m-2">
