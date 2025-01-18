9536f5041647881da1ddfd49af43a993428eb0a5
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-18 14:19:20 +0530

    Logout feature in front end

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
+import {removeUser} from '../utils/userSlice';
+import axios from 'axios';
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const user = useSelector(state => state.user);
+   
+  const handleLogout = async() =>{
+     try {
+         const logoutMessage = await axios.post(`${BASE_URL}/logout`, {}, {withCredentials: true});
+         if(logoutMessage){
+          dispatch(removeUser())
+          navigate('/login')
+         }
+     } catch (error) {
+        console.log("User Logout ::", error)
+     }
+  }
+
     return (
         <>
           <div className="navbar bg-base-300">
@@ -66,7 +84,7 @@ const NavBar = () => {
             </Link>
           </li>
           <li><a>Settings</a></li>
-          <li><a>Logout</a></li>
+          <li><a onClick={handleLogout}>Logout</a></li>
         </ul>
       </div>
     </div>
