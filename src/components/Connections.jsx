6186dfad02088f3b00ec6a3457ce911f676085f2
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-02-21 15:29:11 +0530

    changes

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -6,13 +6,11 @@ import {addConnections} from '../utils/connectionSlice.js';
 
 const Connections = () => {
     const connections = useSelector(state => state.connection);
-    console.log("Line jhhkjhkj", connections)
     const dispatch = useDispatch();
 
     const fetchConnections = async ()=>{
         try {
            const res = await axios.get(`${BASE_URL}/user/connections`, {withCredentials: true});
-           console.log("Linjhb ", res.data.data)
            dispatch(addConnections(res.data.data))
         } catch (error) {
             //Handle Error Case
@@ -28,16 +26,24 @@ const Connections = () => {
     if(connections.length === 0) return <h1>No connections found</h1>
 
     return (
-        <div className='text-center my-10'>
+        <div className='flex flex-col justify-center my-10'>
             <h1 className='text-bold text-2xl'>Connections</h1>
             {connections.map((connect, index)=>{
                 const {firstName, lastName, photoUrl, skills, gender, age, about} = connect;
                  return (
-                    <div className='m-4 p-4 border rounded-lg bg-base-300'>
-
-                      <img alt="photo" className='w-20 h-20' src={photoUrl}/>
-                      <h2>{firstName + " " + lastName}</h2> 
+                    <div className='flex justify-between item-center m-4 p-4 border rounded-lg bg-base-300 w-2/3'>
+                      <div>
+                        <img alt="photo" className='w-20 h-20 rounded-full' src={photoUrl}/>
+                     </div>
+                      <div className='text-left mx-4'>
+                      <h2 className="font-bold text-xl"> {firstName + " " + lastName}</h2> 
+                      <p>{age + " , " + gender}</p>
                       <p>{about}</p>
+                      </div>
+                      <div>
+                      <button className="btn btn-primary">Select</button>
+                      <button className="btn btn-secondary">Reject</button>
+                      </div>
                     </div>
                  )
             })}
--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {addRequests} from '../utils/requestSlice';
 
 const Request = () => {
+    const requests = useSelector(store => store.request);
+    const dispatch = useDispatch();
+
+    const fetchRequest  = async () =>{
+            try {
+                const res = await axios.get(`${BASE_URL}/user/request/recieved`, {withCredentials: true});
+                console.log("linetyyyu", res)
+                dispatch(addRequests(res.data.data))
+                
+            } catch (error) {
+                
+            }
+    }
+
+    useEffect(()=>{
+        fetchRequest();
+    },[])
+    if(!requests) return;
+
+    if(requests.length === 0) return <h1>No requests found</h1>
+
     return (
-        <div>
-            <h1>Request Component</h1>
-            <p>This is the Request component.</p>
+        <div className='flex flex-col justify-center my-10'>
+            <h1 className='text-bold text-2xl'>Qequests</h1>
+            {requests.map((request, index)=>{
+                const {firstName, lastName, photoUrl, skills, gender, age, about} = request.fromUserId;
+                 return (
+                    <div key={index} className='flex m-4 p-4 border rounded-lg bg-base-300 w-1/2'>
+                      <div>
+                        <img alt="photo" className='w-20 h-20 rounded-full' src={photoUrl}/>
+                     </div>
+                      <div className='text-left mx-4'>
+                      <h2 className="font-bold text-xl"> {firstName + " " + lastName}</h2> 
+                      <p>{age + " , " + gender}</p>
+                      <p>{about}</p>
+                      </div>
+                      
+                    </div>
+                 )
+            })}
         </div>
     );
 };
